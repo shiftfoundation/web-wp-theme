7a38598b8db9f688335b408de685e18681159c22
Konstantin Dankov <konstantin.dankov@gmail.com>
2014-07-04 10:56:09 +0300

    Single project view

---
+++ archive-project.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * The Template for displaying all single posts.
+ *
+ * @package Shiftwp
+ */
+
+get_header(); ?>
+
+	// Project
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<div class="boxes">
+				<div class="box"></div>
+				<div class="box"></div>
+				<div class="box">
+					<?php get_template_part( 'content', 'single' ); ?>
+				</div>
+				<div class="box"></div>
+				<div class="box"></div>
+			</div>
+
+
+			<?php shiftwp_post_nav(); ?>
+
+			<?php
+				// If comments are open or we have at least one comment, load up the comment template
+				if ( comments_open() || '0' != get_comments_number() ) :
+					comments_template();
+				endif;
+			?>
+
+		<?php endwhile; // end of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
---
+++ single-project.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * The Template for displaying all single posts.
+ *
+ * @package Shiftwp
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<div class="boxes">
+
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+					<header class="entry-header box w3 h1">
+						<h1><?php the_title(); ?></h1>
+
+						<div class="entry-meta">
+							<?php shiftwp_posted_on(); ?>
+						</div>
+					</header>
+
+					<div class="entry-description box w2 h4">
+						<?php the_content(); ?>
+					</div>
+
+					<div class="entry-content box w4 h5"></div>
+
+				</article>
+
+			</div>
+
+
+			<?php shiftwp_post_nav(); ?>
+
+			<?php
+				// If comments are open or we have at least one comment, load up the comment template
+				if ( comments_open() || '0' != get_comments_number() ) :
+					comments_template();
+				endif;
+			?>
+
+		<?php endwhile; // end of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
