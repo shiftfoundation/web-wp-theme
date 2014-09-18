19018f3438fd5d5bd4765382bb1b721b05cef769
menrva <alex_stanhope@hotmail.com>
2014-09-18 22:47:48 +0100

    Blog updated

--- content-link.php
+++ content-link.php
@@ -3,15 +3,17 @@
  * @package Shiftwp
  */
 ?>
-<article id="post-<?php the_ID(); ?>" class="page hentry">
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
+		<?php echo get_post_meta( get_the_ID(), 'url', true ); ?>
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header>
-	<div class="entry-content">
-		<?php the_content(); ?>
-	</div><!-- .entry-content -->
-	<footer class="entry-footer">
-		<?php shiftwp_posted_on(); ?>
+
+		<div class="entry-meta">
+			<?php shiftwp_posted_on(); ?>
+		</div><!-- .entry-meta -->
+	</header><!-- .entry-header -->
+	<footer class="entry-footer box w2 h4">
 		<?php
 			/* translators: used between list items, there is a space after the comma */
 			$category_list = get_the_category_list( __( ', ', 'shiftwp' ) );
@@ -45,14 +47,17 @@
 			);
 		?>
 
+		<?php edit_post_link( __( 'Edit', 'shiftwp' ), '<span class="edit-link">', '</span>' ); ?>
+	</footer><!-- .entry-footer -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
 		<?php
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . __( 'Pages:', 'shiftwp' ),
 				'after'  => '</div>',
 			) );
 		?>
-
-		<?php edit_post_link( __( 'Edit', 'shiftwp' ), '<span class="edit-link">', '</span>' ); ?>
-	</footer><!-- .entry-footer -->
+	</div><!-- .entry-content -->
 
 </article><!-- #post-## -->
--- index.php
+++ index.php
@@ -20,7 +20,6 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-
 		<?php if ( have_posts() ) : ?>
 
 			<?php /* Start the Loop */ ?>
