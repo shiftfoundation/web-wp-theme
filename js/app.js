c9d4e47440943b05cad7837c9997c9a50e59841b
menrva <alex_stanhope@hotmail.com>
2014-09-28 16:58:07 +0100

    Tabs navigation

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 jQuery(function($){
 
+	$('.subnav').delegate('a[href^="#"]', 'click', function(e){
+		$(this).parent().addClass('selected').siblings().removeClass('selected');
+		window.location = $(this).attr('href');
+		$(window).scrollTop(0);
+		e.preventDefault();
+	});
+
 	function checkLocation(){
 		if ( window.location.hash ) {
 			$('a[href="' + window.location.hash + '"]').click().parent().addClass('selected').siblings().removeClass('selected');
@@ -14,13 +21,6 @@ jQuery(function($){
 		checkLocation();
 		window.onhashchange = checkLocation;
 	}
-
-	$('.subnav').delegate('a[href^="#"]', 'click', function(e){
-		$(this).parent().addClass('selected').siblings().removeClass('selected');
-		window.location = $(this).attr('href');
-		$(window).scrollTop(0);
-		e.preventDefault();
-	});
 
 	$('.menu-toggle').click(function(){
 		$('#masthead').toggleClass('active');
