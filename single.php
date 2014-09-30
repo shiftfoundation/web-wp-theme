eb0e8d753aed2a18576fad219a4a2abafc1a9504
menrva <alex_stanhope@hotmail.com>
2014-09-30 11:28:21 +0100

    Related posts

--- single.php
+++ single.php
@@ -14,6 +14,42 @@ get_header(); ?>
 
 			<?php get_template_part( 'content', 'single' ); ?>
 
+<?php 
+
+$post_terms = wp_get_object_terms(get_the_ID(), 'issues', array('fields'=>'ids'));
+
+$args = array(
+	'post_type' => 'post',
+	'post_status' => 'publish',
+	'posts_per_page' => 5,
+	'post__not_in' => array( get_the_ID() ),
+    'tax_query' => array(
+        array(
+            'taxonomy' => 'issues',
+            'field' => 'id',
+            'terms' => $post_terms
+        )
+    )
+);
+
+$the_query = new WP_Query( $args );
+
+if ( $the_query->have_posts() ) { ?>
+			<article class="hentry">
+				<h2>Related posts</h2>
+				<br>
+				</article>
+<?php
+    while ( $the_query->have_posts() ) {
+        $the_query->the_post();
+				get_template_part( 'content', get_post_format() );
+    }
+}
+wp_reset_postdata();
+
+
+			?>
+
 			<?php
 				// If comments are open or we have at least one comment, load up the comment template
 				if ( comments_open() || '0' != get_comments_number() ) :
