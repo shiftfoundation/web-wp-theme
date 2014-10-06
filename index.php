e38a627d2e9274630279705bc2ad8b2da59de380
menrva <alex_stanhope@hotmail.com>
2014-10-06 08:28:47 +0100

    Comment section

--- index.php
+++ index.php
@@ -20,18 +20,12 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+		<?php /* ?>
 		<?php if ( have_posts() ) : ?>
 
-			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-				?>
+				<?php get_template_part( 'content', get_post_format() ); ?>
 
 			<?php endwhile; ?>
 
@@ -42,6 +36,9 @@ get_header(); ?>
 			<?php get_template_part( 'content', 'none' ); ?>
 
 		<?php endif; ?>
+		<?php */ ?>
+
+		<?php echo do_shortcode('[ajax_load_more post_type="post" posts_per_page="10" max_pages="10"]'); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
